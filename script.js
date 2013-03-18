09c300a4cdc6749c50a166e2d3467d6ab30b6149
jackchi <fuching.chi@gmail.com>
2013-03-18 10:36:46 -0700

    First Commit: Interview Questions & Test Code

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+$(document).ready(function() {
+
+	var connectionParams={};
+    connectionParams.apiKey = "9011b0381d844d3c95574cdcb26cf3e8";
+    connectionParams.server = "http://sandbox.bunchball.net/nitro/json"; 
+                       
+    var nitro = new Nitro(connectionParams); 
+
+    
+});
+
+*/
